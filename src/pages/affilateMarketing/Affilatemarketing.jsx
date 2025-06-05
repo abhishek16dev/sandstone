7fbee032c079d21901715ace9b8149264524da02
Abhishek singh <rajputeabhishek982@gmail.com>
2025-06-05 13:01:41 +0530

    code added

---
+++ src/pages/affilateMarketing/Affilatemarketing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Affilatemarketing = () => {
+  return (
+    <div className='mt-[120px]'>
+      <p>affmarketing</p>
+    </div>
+  )
+}
+
+export default Affilatemarketing
